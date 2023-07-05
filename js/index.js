8a429f67a60f76e1409a130dac587ea849c3a36b
veritozy <121317594+veritozy@users.noreply.github.com>
2023-07-04 19:19:36 -0500

    Add files via upload
    Publicar sitio boutique.

---
+++ js/index.js
@@ -0,0 +1,36 @@
+let miNombre = "Verónica";
+console.log(miNombre);
+let a;
+let b=6;
+
+console.log(a);
+console.log(b);
+
+console.log(typeof b);
+
+console.log("Ingrese dos números");
+b-=2;
+console.log(b);
+
+console.log("Mi nombre es "+miNombre);
+
+console.log(miNombre.length)
+
+let programa = "Javascript";
+
+console.log(programa[0]);
+
+
+
+// let suma = num1+num2;
+
+
+console.log(`Mi nombre es ${miNombre}`);
+
+const n = 4;
+
+console.log(n);
+
+n=7;
+console.log(n);
+
